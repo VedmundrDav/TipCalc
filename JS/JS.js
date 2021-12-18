67cb8c6ea978dfe448d3de985cac4ec20afeaafd
Daniel <iban1031@gmail.com>
2021-12-18 16:24:55 -0600

    Added error checking and added conditions for if the tip is over a certain amount and below a certain amount. Added a 'tip field' that will show how much the tip is.

--- JS/JS.js
+++ JS/JS.js
@@ -3,25 +3,43 @@ function calc() {
     const percentOffset = 100;
     var billAmount = document.getElementById("billAmount").value;
     var tipAmount = document.getElementById("tipAmount").value;
+    var tip = document.getElementById("tipAmountField").value;
     var totalAmount = document.getElementById("ttlAmount").value;
 
-    if(tipAmount > 20){
-        confirm("Tip is large. Was the service that good?");
+    if (billAmount <= 0) {
+        alert("Enter a valid value for the bill.");
+    } else if(tipAmount < 0){
+        alert("Enter a valid value for the tip.");
+    }else{
+        if (tipAmount > 20) {
+            confirm("Bravo, a generous tip!");
+        } else if (tipAmount < 15) {
+            confirm("Please consider the waiter/server. This tip is rather low.");
+        }
+
+        //MATH
+        totalAmount = parseFloat(billAmount) + (parseFloat(billAmount) * (parseFloat(tipAmount) / parseFloat(percentOffset)));
+        tip = parseFloat(billAmount) * (parseFloat(tipAmount) / parseFloat(percentOffset));
+        //END MATH
+
+        //SET FIELDS
+        var totalAmountField = document.getElementById("ttlAmount");
+        totalAmountField.value = totalAmount.toFixed(2);
+
+        var tipField = document.getElementById("tipAmountField");
+        tipField.value = tip.toFixed(2);
+        //END SET FIELDS
     }
-
-    totalAmount = parseInt(billAmount) + (parseInt(billAmount) * (parseInt(tipAmount) / parseInt(percentOffset)));
-
-    var totalAmountField = document.getElementById("ttlAmount");
-    totalAmountField.value = totalAmount.toFixed(2);
-
 }
 
 function clearFields(){
     var bill = document.getElementById("billAmount");
-    var tip = document.getElementById("tipAmount");
+    var tipTtl = document.getElementById("tipAmount");
+    var tip = document.getElementById("tipAmountField");
     var amount = document.getElementById("ttlAmount");
 
     bill.value = 0.00;
+    tipTtl.value = 0;
     tip.value = 0;
     amount.value = 0.00;
 
